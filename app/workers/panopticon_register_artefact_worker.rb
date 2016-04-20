9e10c2b7b49f8e2467a694657c91b8c4706459de
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2016-04-20 16:25:10 +0100

    WIP

--- app/workers/panopticon_register_artefact_worker.rb
+++ app/workers/panopticon_register_artefact_worker.rb
@@ -4,7 +4,7 @@ require 'gds_api/panopticon'
 class PanopticonRegisterArtefactWorker < WorkerBase
   sidekiq_options queue: :panopticon
 
-  def perform(edition_id, options = {})
+  def perform_with_request_tracing(edition_id, options = {})
     edition = Edition.find(edition_id)
 
     if edition.present?
--- app/workers/worker_base.rb
+++ app/workers/worker_base.rb
@@ -1,7 +1,24 @@
 class WorkerBase
   include Sidekiq::Worker
 
+  def self.perform_async(*args)
+    args << request_id_argument
+    super(*args)
+  end
+
   def self.perform_async_in_queue(queue, *args)
+    args << request_id_argument
     client_push('class' => self, 'args' => args, 'queue' => queue || get_sidekiq_options["queue"])
+  end
+
+  def self.request_id_argument
+    {request_id: GdsApi::GovukHeaders.headers[:govuk_request_id]}
+  end
+  private_class_method :request_id_argument
+
+  def perform(*args)
+    request_id = args.pop["request_id"]
+    GdsApi::GovukHeaders.set_header(:govuk_request_id, request_id)
+    perform_with_request_tracing(*args)
   end
 end
