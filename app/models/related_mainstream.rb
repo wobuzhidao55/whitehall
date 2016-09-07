74a8974913089b3651e8160143b5c75cdb431d7b
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-09-07 17:09:01 +0000

    Add validations on Related Mainstream model

--- app/models/related_mainstream.rb
+++ app/models/related_mainstream.rb
@@ -1,3 +1,5 @@
 class RelatedMainstream < ActiveRecord::Base
   belongs_to :edition
+  validates :content_id, presence: true, uniqueness: true
+  validates :edition_id, presence: true
 end
---
+++ test/unit/models/related_mainstream_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class RelatedMainstreamTest < ActiveSupport::TestCase
+  test "raises an error if creating a record with a nil content_id" do
+    detailed_guide = create(:detailed_guide)
+    assert_raises "cannot create Related Mainstream record with nil content_id" do
+      RelatedMainstream.create!(edition_id: detailed_guide.id, content_id: nil)
+    end
+  end
+
+  test "raises an error if creating a record with an existing content_id" do
+    detailed_guide = create(:detailed_guide)
+    RelatedMainstream.create(edition_id: detailed_guide.id, content_id: "5a2fea6a-360a-49ba-97b3-46d3612ec198")
+    second_detailed_guide = create(:detailed_guide)
+
+    assert_raises "cannot create Related Mainstream record with duplicate content_id" do
+      RelatedMainstream.create!(edition_id: second_detailed_guide.id, content_id: "5a2fea6a-360a-49ba-97b3-46d3612ec198")
+    end
+    assert_equal 1, RelatedMainstream.count
+  end
+
+  test "raises an error if creating a record with a nil edition" do
+    assert_raises "cannot create Related Mainstream record with a nil edition_id" do
+      RelatedMainstream.create!(edition_id: nil, content_id: "5a2fea6a-360a-49ba-97b3-46d3612ec198")
+    end
+  end
+end
